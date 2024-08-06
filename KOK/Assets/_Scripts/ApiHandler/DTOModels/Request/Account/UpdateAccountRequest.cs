7ecbee9ced1df602e26774c8dcd79bb6bdb5b6bb
Cậu vàng <huuminhgm@gmail.com>
2024-08-06 20:07:07 +0700

    almost complete profile

--- KOK/Assets/_Scripts/ApiHandler/DTOModels/Request/Account/UpdateAccountRequest.cs
+++ KOK/Assets/_Scripts/ApiHandler/DTOModels/Request/Account/UpdateAccountRequest.cs
@@ -6,11 +6,11 @@ namespace KOK.ApiHandler.DTOModels
     public class UpdateAccountRequest
     {
         public string UserName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        //public string Email { get; set; } = string.Empty;
         public string Gender { get; set; } = AccountGender.OTHERS.ToString();
         public decimal UpBalance { get; set; } = 0;
-        public int? Yob { get; set; } = 2000;
-        public string PhoneNumber { get; set; } = null;
+        //public int? Yob { get; set; } = 2000;
+        public string PhoneNumber { get; set; } = string.Empty;
         public Guid? CharacterItemId { get; set; } = null;
         public Guid? RoomItemId { get; set; } = null;
 
